a3b13982c6d72fb60dc98b20e4602f2b3a3ff15c
Tassio Silva <tassio.s13@gmail.com>
2019-11-03 01:16:01 -0300

    Subindo alterações json

--- init.py
+++ init.py
@@ -2,25 +2,10 @@
 # -*- coding: utf-8 -*-
 import utils
 
-def iniciaDir():
-    #checando se existe diretorio definido
-    diretorio = utils.checkDir()
-    
-    #Se nao existir diretorio: Seleciona um novo. Se existir: Confirme se está correto.
-    if not diretorio:
-        utils.saveDir(utils.selectDir())
-    else:
-        diretorio = utils.confirmaDir(diretorio)
-    
-    return diretorio
-    
-#listaItens = utils.listaMidias(diretorio)
     
 def main():
-    diretorio = iniciaDir()
+    utils.iniciaDir()
     
-    
-
 #Define a função main
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 from tkinter import filedialog,messagebox,Tk
 import os
+import json
+import urllib.request
+import socket
 
 #Selecionar diretorio
 def selectDir():
@@ -13,30 +16,66 @@ def selectDir():
 
 #Conferir se ja existe um diretorio
 def checkDir():
-    arq = open("dir.txt","r")
-    txt = arq.read()
-    arq.close()
-    if not txt:
-        return False
+    dados = abrirJson()
+    #Se nao existir diretorio: Seleciona um novo. Se existir: Confirme se está correto.
+    if not dados['filial'][getFilialPos()]['diretorio']:
+        saveDir(selectDir())
     else:
-        return txt
+        confirmaDir(dados)
 
 #Salvar novo diretorio
-def saveDir(path):
-    arq = open("dir.txt","w")
-    arq.write(path)
+def saveDir(path):   
+    dados = abrirJson()
+    dados['filial'][getFilialPos()]['diretorio'] = path
+    salvarJson(dados)
+    """arq = open("dir.txt","w")
+    arq.write(path)"""
 
-def confirmaDir(path):
+def confirmaDir(dados):
     root = Tk()
     root.withdraw()
-    MsgBox = messagebox.askquestion ('Confirmar diretório','O diretorio para compartilhamento de mídias esta correto?\n'+path,icon = 'warning')
+    MsgBox = messagebox.askquestion ('Confirmar diretório','O diretorio para compartilhamento de mídias esta correto?\n'+dados['filial'][getFilialPos()]['diretorio'],icon = 'warning')
     if MsgBox == 'no':
         messagebox.showinfo('Selecionar novo diretório','Favor selecionar o diretório correto.')
         txt = selectDir()
         saveDir(txt)
-        return txt
-    else: return path
+
     
 def listaMidias(path):
     lista = os.listdir(path)
     return lista
+
+def abrirJson():
+    with open('dados.json','r') as json_file:
+        jsonL = json.load(json_file)
+        json_file.close()
+        return jsonL
+    
+def salvarJson(dados):
+    with open ('dados.json','w') as json_file:
+        json.dump(dados,json_file)
+        json_file.close()
+
+def preencheMidiasJson():
+    jsonL = abrirJson()
+    jsonL['filial'][getFilialPos()]['midias'] = listaMidias(jsonL['filial'][getFilialPos()]['diretorio'])
+    salvarJson(jsonL)
+    
+
+def iniciaDir():
+    #checando se existe diretorio definido
+    checkDir()
+    preencheMidiasJson()
+  
+def getIP():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 1))
+    ip_local = s.getsockname()[0]
+    return ip_local
+
+def getFilialPos():
+    jsonL = abrirJson()
+    for i,filial in enumerate(jsonL['filial']):
+        if filial['ip']  ==  getIP():
+            return i
+            
